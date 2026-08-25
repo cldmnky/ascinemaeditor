5f9951ee732df00039a52de56a5dbfb8f0c404c0
mbengtss <mbengtss@redhat.com>
2026-08-25 18:01:22 +0200

    Bind static server to loopback and harden file serving

--- main.go
+++ main.go
@@ -2,30 +2,85 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
+	"time"
 )
+
+// restrictedFileSystem wraps http.Dir to block dotfiles, dot directories
+// (.git in particular) and directory listings. Directories that contain an
+// index.html are still served normally so "/" resolves to index.html.
+type restrictedFileSystem struct {
+	root http.FileSystem
+}
+
+func (r restrictedFileSystem) Open(name string) (http.File, error) {
+	clean := path.Clean("/" + name)
+	for _, segment := range strings.Split(clean, "/") {
+		if strings.HasPrefix(segment, ".") {
+			return nil, fs.ErrPermission
+		}
+	}
+
+	file, err := r.root.Open(clean)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
+	if info.IsDir() {
+		index, indexErr := r.root.Open(path.Join(clean, "index.html"))
+		if index != nil {
+			index.Close()
+		}
+		if indexErr != nil {
+			file.Close()
+			return nil, fs.ErrNotExist
+		}
+	}
+
+	return file, nil
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "127.0.0.1"
+	}
 
-	// Get the directory where the executable is located
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	root, err := filepath.Abs(".")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Serve static files from the current directory
-	fs := http.FileServer(http.Dir("."))
-	http.Handle("/", fs)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(restrictedFileSystem{root: http.Dir(root)}))
 
-	fmt.Printf("Starting server on http://localhost:%s\n", port)
-	fmt.Printf("Serving files from: %s\n", dir)
+	server := &http.Server{
+		Addr:              net.JoinHostPort(host, port),
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      30 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
 
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	fmt.Printf("Starting server on http://%s\n", server.Addr)
+	fmt.Printf("Serving files from: %s\n", root)
+
+	log.Fatal(server.ListenAndServe())
 }
